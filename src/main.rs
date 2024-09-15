859e9613507e9dee4c1337a853bce93bb85926c6
Pokyshac <sabzirovi@mail.ru>
2024-09-15 22:38:54 +0400

    Add console output for first task

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,10 @@ use tasks::task_1;
 mod tasks;
 
 fn main() {
-    let m = 3;
+    let m = 5;
     let mult_table = task_1::get_multiplication_table(m);
     let add_table = task_1::get_adding_table(m);
 
-    for i in 0..mult_table.len() {
-        println!("{} | ", mult_table[i]);
-    }
+    let output_table = task_1::table_to_string(&mult_table, m).unwrap();
+    println!("{}", output_table);
 }
