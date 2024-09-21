24e56e02eac9fcf672f7dd6518e702b135440935
Pokyshac <sabzirovi@mail.ru>
2024-09-21 14:55:15 +0400

    Done task 2

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,21 @@
 )]
 
 use tasks::task_1;
+use tasks::task_2;
 
 mod tasks;
 
 fn main() {
-    let m = 5;
-    let mult_table = task_1::get_multiplication_table(m);
-    let add_table = task_1::get_adding_table(m);
+    // let m = 5;
+    // let mult_table = task_1::get_multiplication_table(m);
+    // let add_table = task_1::get_adding_table(m);
 
-    let output_table = task_1::table_to_string(&mult_table, m).unwrap();
-    println!("{}", output_table);
+    // let output_table = task_1::table_to_string(&mult_table, m as usize).unwrap();
+    // println!("{}", output_table);
+
+    let a = -10;
+    let b = 1;
+    let m = 9;
+    let system = task_2::get_moved_deduction_system(a, b, m);
+    task_2::print_deduction_system(&system);
 }
--- src/tasks/task_1.rs
+++ src/tasks/task_1.rs
@@ -1,6 +1,7 @@
 use std::ops::DerefMut;
 
-pub fn get_multiplication_table(m: usize) -> Vec<usize> {
+pub fn get_multiplication_table(m: u64) -> Vec<usize> {
+    let m = m as usize;
     let mut table = vec![0; m * m];
     for i in 0..m {
         for j in i..m {
@@ -12,7 +13,8 @@ pub fn get_multiplication_table(m: usize) -> Vec<usize> {
     table
 }
 
-pub fn get_adding_table(m: usize) -> Vec<usize> {
+pub fn get_adding_table(m: u64) -> Vec<usize> {
+    let m = m as usize;
     let mut table = vec![0; m * m];
     for i in 0..m {
         for j in i..m {
