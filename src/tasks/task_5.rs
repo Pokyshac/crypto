163d5fcdd3a6440862af9524decd5034208d9654
Pokyshac <sabzirovi@mail.ru>
2024-10-18 18:38:23 +0400

    Corrected mistake in task 5

--- src/tasks/task_5.rs
+++ src/tasks/task_5.rs
@@ -4,22 +4,24 @@ use crate::tasks::task_3::calculate_eulers_function;
 pub fn remainder(a: i64, k: i64, m: i64) -> Result<i64, String> {
     let phi = match calculate_eulers_function(m) {
         Ok(x) => x,
-        Err(error) => return Err(String::from("Argument 'm' must be greater than zero"))
+        Err(error) => return Err(error) 
     };
     let mut d = modulo(a, m).1;
-    let mut r = modulo(k, phi).1;    
-    let mut result = 1;
-    if r % 2 != 0 {
-        r -= 1;
-        result = d;
-    }
+    let mut c = modulo(k, phi).1;
+    let mut t = 1;
     
-    while r > 1 {
+    while c > 1 {
+        if c % 2 != 0 {
+            c -= 1;
+            t *= d;
+            t = modulo(t, m).1;
+        }
+        
         d = modulo(d * d, m).1;
         
-        r /= 2;
+        c /= 2;
     }
-    result = modulo(result * d, m).1;
+    let result = modulo(t * d, m).1;
 
     Ok(result)
 }
