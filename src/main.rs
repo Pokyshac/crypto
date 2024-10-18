69c8b139f361e0b0441fc41d6080169df37496e1
Pokyshac <sabzirovi@mail.ru>
2024-10-18 20:32:47 +0400

    Done task 6

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@
     dead_code
 )]
 
+use std::io::Write;
+use std::process::Stdio;
+
+use num::iter::Range;
 use tasks::task_1;
 use tasks::task_2;
 use tasks::task_3;
 use tasks::task_4;
 use tasks::task_5;
+use tasks::task_6;
 use tasks::utils;
 
 use crate::tasks::utils::get_prime_factors;
@@ -33,9 +38,9 @@ fn main() {
     // let n = 5;
     // println!("{}", calculate_eulers_function(n).unwrap());
 
-    // let a = 7;
-    // let k = 2024;
-    // let m = 13;
+    // let a = 17;
+    // let k = 2001;
+    // let m = 1000;
     // let result =  task_5::remainder(a, k, m);
     // match result {
     //     Ok(x) => println!("{}", x),
@@ -45,23 +50,21 @@ fn main() {
     // let m = 48;
     // println!("{}", task_4::find_eulers_function_arguments(m).unwrap().len());
 
-    for i in 2..128 {
-        let v = task_4::find_eulers_function_arguments(i).unwrap().len();
+    let mut alphabet = task_6::Alphabet::new();
+    for i in 97..110 {
+        alphabet.insert(char::from_u32(i as u32).unwrap(), i - 96);
+    }   
+    alphabet.insert(' ', 15);
 
-        let mut cnt = 0;
-        let mut n = 1;
-        while n <= 10000 {
-            let phi_n = task_3::calculate_eulers_function(n).unwrap();
-            if phi_n == i {
-                cnt += 1;
-            }
+    let text = String::from("abd ca aba");
+    println!("{}", text);
+    println!("-----------------");
 
-            n += 1;
-        }
+    let (open_key, hidden_key) = task_6::get_rsa_keys(19, 41);
 
-        if v != cnt {
-            println!("govno v {}", i);
-            panic!("{} != {}", v, cnt);
-        }
-    }
+    let encoded_text = task_6::rsa_encode(&open_key, &text, &alphabet);
+    let decoded_text = task_6::rsa_decode(&hidden_key, &encoded_text, &alphabet);
+
+    println!("{:?}", encoded_text);
+    println!("{:?}", decoded_text);
 }
---
+++ src/tasks/task_6.rs
@@ -0,0 +1,89 @@
+use crate::tasks::task_3::calculate_eulers_function;
+use crate::tasks::task_5::remainder;
+use crate::tasks::utils;
+
+use std::collections::HashMap;
+
+pub struct OpenKey {
+    exp: i64,
+    n: i64
+}
+
+pub struct HiddenKey {
+    exp: i64,
+    n: i64
+}
+
+pub struct Alphabet {
+    pairs: HashMap<char, i64>,
+    reverse_pairs: HashMap<i64, char>
+}
+
+impl Alphabet {
+    pub fn new() -> Self {
+        Self {
+            pairs: HashMap::new(),
+            reverse_pairs: HashMap::new()
+        }
+    }
+
+    pub fn insert(&mut self, symbol: char, value: i64) -> bool {
+        if !self.pairs.contains_key(&symbol) && !self.reverse_pairs.contains_key(&value) {
+           self.pairs.insert(symbol, value);
+           self.reverse_pairs.insert(value, symbol);
+
+           return true;
+       }
+       else {
+           false
+       }
+    }
+
+    pub fn get_char(&self, value: &i64) -> Option<&char> {
+        self.reverse_pairs.get(value)
+    }
+
+    pub fn get_value(&self, symbol: &char) -> Option<&i64> {
+        self.pairs.get(symbol)
+    }
+}
+
+pub fn get_rsa_keys(p: i64, q: i64) -> (OpenKey, HiddenKey) {
+    let n = p * q;
+    let phi = (p - 1) * (q - 1); 
+    let open_exp = phi - 1;
+    let (a, b) = utils::euclidean_algorithm(phi, open_exp);
+    let hidden_exp = phi - i64::abs(i64::min(a, b));
+
+    let open_key = OpenKey { exp: open_exp, n };
+    let hidden_key = HiddenKey { exp: hidden_exp, n };
+
+    (open_key, hidden_key)
+}
+
+pub fn rsa_encode(open_key: &OpenKey, text: &String, alphabet: &Alphabet) -> Vec<i64> {
+
+    let mut encoded_text = Vec::with_capacity(text.len());
+    let mut text_chars = text.chars().into_iter();
+    
+    for symbol in text_chars {
+        let char_value = alphabet.get_value(&symbol).unwrap();
+        let encoded_char_id = remainder(*char_value, open_key.exp, open_key.n).unwrap();
+        encoded_text.push(encoded_char_id);
+    }
+    
+    encoded_text
+}
+
+pub fn rsa_decode(hidden_key: &HiddenKey, text: &Vec<i64>, alphabet: &Alphabet) -> String {
+
+    let mut decoded_text = String::with_capacity(text.len());
+    
+    for symbol in text.iter() {
+        let decoded_char_id = remainder(*symbol, hidden_key.exp, hidden_key.n).unwrap();
+        let decoded_char = alphabet.get_char(&decoded_char_id).unwrap();
+        decoded_text.push(*decoded_char);
+    }
+    
+    decoded_text
+}
